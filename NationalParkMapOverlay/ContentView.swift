84ac6988d456806498060a3102ea429272daf6f8
Sreekuttan Sudarsanan <kuttansree172@gmail.com>
2020-12-30 21:45:44 -0500

    Initial Commit

---
+++ NationalParkMapOverlay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NationalParkMapOverlay
+//
+//  Created by Sreekuttan Sudarsanan on 12/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NationalParkMapOverlay/NationalParkMapOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationalParkMapOverlayApp.swift
+//  NationalParkMapOverlay
+//
+//  Created by Sreekuttan Sudarsanan on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct NationalParkMapOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
