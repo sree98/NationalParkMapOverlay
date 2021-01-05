2ac0ba433d53b484021bb7b11fb729912f9f3ecf
Sreekuttan Sudarsanan <kuttansree172@gmail.com>
2021-01-05 02:23:41 -0500

    updated mapview with annotations with picture

--- NationalParkMapOverlay/MapView.swift
+++ NationalParkMapOverlay/MapView.swift
@@ -19,13 +19,12 @@ class ParkAnnotation : NSObject, MKAnnotation {
         self.coordinate = coordinate
         self.title = title
         self.subtitle = subtitle
-        self.image = UIImage(imageLiteralResourceName: image)
+        self.image = UIImage(named: image)!
     }
 
 }
 
 class ParkAnnotationView: MKAnnotationView {
-    //No subclassing needed...
 }
 
 struct MapView: UIViewRepresentable {
@@ -37,15 +36,26 @@ struct MapView: UIViewRepresentable {
     //Define a class which works as a delegate
     class MyMapViewDelegate: NSObject, MKMapViewDelegate {
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId, for: annotation) as! ParkAnnotationView
-            if let observationAnnotation = annotation as? ParkAnnotation {
-                
-                annotationView.image = observationAnnotation.image
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: MapView.reuseId, for: annotation) as! ParkAnnotationView
+            
+            if let parkAnnotation = annotation as? ParkAnnotation {
+
+                let imageLayer = CALayer()
+                imageLayer.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+                imageLayer.contents = parkAnnotation.image.cgImage
+                imageLayer.cornerRadius = imageLayer.frame.size.width / 2
+                imageLayer.masksToBounds = true
+                imageLayer.borderWidth = 4.0
+                imageLayer.borderColor = UIColor.white.cgColor
+                annotationView.layer.addSublayer(imageLayer)
+                annotationView.frame = imageLayer.frame
+
             }
+            
             annotationView.canShowCallout = true
             
-            print("Here")
-            
+            annotationView.leftCalloutAccessoryView = UIImageView(image: #imageLiteral(resourceName: "apple"))
+
             return annotationView
         }
     }
@@ -75,59 +85,6 @@ struct MapView: UIViewRepresentable {
     }
     //...
 }
-
-
-//struct MapView: UIViewRepresentable{
-//
-//    let locationManager = CLLocationManager()
-//
-////    var locValue : CLLocationCoordinate2D
-//
-//    class Coordinator: NSObject, MKMapViewDelegate {
-//        var parent:MapView
-//
-//        init(_ parent: MapView) {
-//            self.parent = parent
-//        }
-//
-//        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
-//            print(mapView.centerCoordinate)
-//        }
-//
-//        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-//            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
-//            view.canShowCallout = true
-//            return view
-//        }
-//
-//    }
-//
-//    func makeCoordinator() -> Coordinator {
-//        Coordinator(self)
-//    }
-//
-//
-//    func makeUIView(context: Context) -> MKMapView {
-//        let mapView = MKMapView(frame: .zero)
-//        mapView.delegate = context.coordinator
-//
-//        let annotation = ParkAnnotation(location: CLLocationCoordinate2D(latitude: 37.83, longitude: -119.5))
-//        annotation.title = "Yosemite"
-//        annotation.subtitle = "National Park"
-//        mapView.addAnnotation(annotation)
-//
-//        return mapView
-//    }
-//
-//    func updateUIView(_ view: MKMapView, context: Context){
-//            let coordinate = CLLocationCoordinate2D(
-//                latitude: 40, longitude: -75)
-//            let span = MKCoordinateSpan(latitudeDelta: 10.0, longitudeDelta: 5.0)
-//            let region = MKCoordinateRegion(center: coordinate, span: span)
-//            view.setRegion(region, animated: true)
-//    }
-//
-//}
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
